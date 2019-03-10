cd59258a1f36355055911156ed94f2958893ed3f
JasonJieJay <47524204+JasonJieJay@users.noreply.github.com>
2019-03-10 22:21:34 +0800

    Update CSV增删改查.py

--- CSV增删改查.py
+++ CSV增删改查.py
@@ -28,60 +28,100 @@ def 不存在():
     return name
 
 def 查询(): 
-    name=存在()
-    with open('/home/jason/python/test1.csv','r') as f:
-        reader=f.readlines()
-        for i in reader:
-            i=i.strip('\n')
-            if i[0] == name:
-                print(i)
-def 修改():
-    name=存在()
-    with open('/home/jason/python/test1.csv','r') as f:
-        reader=f.readlines()
-    with open('/home/jason/python/test1.csv','w') as x:
-        file=csv.writer(x,dialect='excel')
-        for l in reader:
-            if l[0] != name:
-                l=l.strip('\n')
-                l=l.split(',')
-                file.writerow(l)
+    while True:
+        name=存在()
+        with open('/home/jason/python/test1.csv','r') as f:
+            reader=f.readlines()
+            for i in reader:
+                i=i.strip('\n')
+                if i[0] == name:
+                    print('名字:',i[0],'   电话:',i[2])               
+            jx=input('输入任意键继续/输入q退出：')
+            if jx!='q':
+                print('请继续查询!')
             else:
-                num=input('请输入tel:')
-                um1=[name,num]
-                file.writerow(um1)
-                print('修改成功！')
+                print('退出查询！')
+                break
+def 修改():
+    while True:
+        name=存在()
+        with open('/home/jason/python/test1.csv','r') as f:
+            reader=f.readlines()
+        with open('/home/jason/python/test1.csv','w') as x:
+            file=csv.writer(x,dialect='excel')
+            for l in reader:
+                if l[0] != name:
+                    l=l.strip('\n')
+                    l=l.split(',')
+                    file.writerow(l)
+                else:
+                    num=input('请输入tel:')
+                    um1=[name,num]
+                    file.writerow(um1)
+                    print('修改成功！')
+            jx=input('输入任意键继续/输入q退出：')
+            if jx!='q':
+                print('请继续查询!')
+            else:
+                print('退出查询！')
+                break
 
 def 删除():
-    name=存在()
-    with open('/home/jason/python/test1.csv','r') as f:
-        reader=f.readlines()
-    with open('/home/jason/python/test1.csv','w') as d:
-        for a in reader:
-            if name != a[0]:
-                d.write(a)
-        print('删除成功')
+    while 1:
+        name=存在()
+        with open('/home/jason/python/test1.csv','r') as f:
+            reader=f.readlines()
+        with open('/home/jason/python/test1.csv','w') as d:
+            for a in reader:
+                if name != a[0]:
+                    d.write(a)
+            print('删除成功')
+            jx=input('输入任意键继续/输入q退出：')
+            if jx!='q':
+                print('请继续查询!')
+            else:
+                print('退出查询！')
+                break
 
 def 新增():
-    name=不存在()
-    num=input('请输入tel:')
-    with open('/home/jason/python/test1.csv','a') as x:
-        file=csv.writer(x,dialect='excel')
-        list1=[name,num]
-        file.writerow(list1)
-    print('新增成功')
+    while 1:
+        name=不存在()
+        num=input('请输入tel:')
+        with open('/home/jason/python/test1.csv','a') as x:
+            file=csv.writer(x,dialect='excel')
+            list1=[name,num]
+            file.writerow(list1)
+        print('新增成功')
+        jx=input('输入任意键继续/输入q退出：')
+        if jx!='q':
+            print('请继续查询!')
+        else:
+            print('退出查询！')
+            break
 
 def fun1():
     while True:
-        type1=int(input('新增输入:1；删除输入:2；修改输入:3；查询输入:4\n'))
-        if type1==4:
+        print('*'*26)
+        print('''
+        新增输入:z
+        删除输入:s
+        修改输入:x
+        查询输入:c
+        退出输入:q
+        ''')
+        print('*'*26)
+        type1=input('请选择操作方式：')
+        if type1=='c':
             查询()
-        elif type1==3:
+        elif type1=='x':
             修改()
-        elif type1==2:
+        elif type1=='s':
             删除()
-        elif type1==1:
+        elif type1=='z':
             新增()
+        elif type1=='q':
+            print('退出程序成功！')
+            break
         else:
             print('输入有误！重新输入！')
 
