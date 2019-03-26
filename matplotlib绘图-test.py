163d9e814f2d9ebe41e701ce17ec74cc1476e877
JasonJieJay <47524204+JasonJieJay@users.noreply.github.com>
2019-03-26 17:45:04 +0800

    Update matplotlib绘图-test.py

--- matplotlib绘图-test.py
+++ matplotlib绘图-test.py
@@ -10,7 +10,7 @@ plt.rcParams['axes.unicode_minus'] = False#显示负数
 x = np.arange(0,4,0.01)
 y = np.sin(2*np.pi*x)#曲线函数
 plt.figure(figsize=(10,6))#改变图片大小
-plt.plot(x,y,'r--',linewidth=1)#绘制线状图
+plt.plot(x,y,'g--',alpha=0.5,linewidth=3,)#绘制线状图;alpha为透明度，g--绿色虚线图
 plt.xlim(0,4)#改变x轴的显示区域
 plt.ylim(-1,1)#改变y轴的显示区域
 plt.grid()#显示网格
@@ -19,5 +19,5 @@ plt.ylabel('电压(V)',fontsize=12)#y轴的标签
 plt.title('电压变化趋势图',fontsize=15)#标题
 plt.text(1.5,0.25,'y=sin(2*pi*x)',fontsize=12)#插入文本
 plt.text(1,0.75,'学个锤子',fontsize=30)#插入文本
-plt.show()#显示图片
-plt.savefig('/home/jason/zelin/数据分析/ChuiziDianya.png',dpi=400,bbox_inches='tight')#保存
+plt.savefig('/home/jason/zelin/数据分析/ChuiziDianya.png',dpi=400,bbox_inches='tight')#保存 在show前保存
+plt.show()#显示图片 show（）之后为空白页
