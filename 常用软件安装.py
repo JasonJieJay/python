7ef43e29a00d6c83e35feb8213a45d69f59e2e71
机器学习 <47524204+JasonJieJay@users.noreply.github.com>
2019-04-28 10:54:30 +0800

    Create 常用软件安装.py

---
+++ 常用软件安装.py
@@ -0,0 +1,36 @@
+pip3的安装、升级、卸载
+    sudo apt-get update
+    安装：sudo apt-get install python3-pip
+    升级：sudo pip3 install --upgrade pip
+    卸载：sudo apt-get remove python3-pip
+    
+谷歌 Chrome 浏览    
+     sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
+    启动：/usr/bin/google-chrome-stable
+
+    
+    
+jupyter安装
+    sudo pip3 install jupyter
+    启动：jupyter notebook
+    先卸载当前安装的jupyter notebook
+          sudo pip install pip-autoremove
+          sudo pip-autoremove jupyter -y
+
+
+
+mysql
+      下载deb包
+          https://dev.mysql.com/downloads/file/?id=482330
+          sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb
+              选8.0版本——选8.0版本——OK
+          sudo apt update
+       安装mysql8
+          sudo apt install mysql-server
+              设置密码
+              密码加密方式选择5.x
+      查看mysql字符集，mysql8字符集默认为utf-8
+           show variables like '%char%'
