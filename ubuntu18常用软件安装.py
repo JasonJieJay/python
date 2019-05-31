d782d34392fed7976335e5fc9304221c8a91b421
机器学习 <47524204+JasonJieJay@users.noreply.github.com>
2019-05-31 14:56:01 +0800

    Update ubuntu18常用软件安装.py

--- ubuntu18常用软件安装.py
+++ ubuntu18常用软件安装.py
@@ -75,6 +75,8 @@ anaconda安装：
             安装链接：https://cloud.tencent.com/developer/article/1162755?from=10680
             下载链接（Anaconda3-5.1.0-Linux-x86_64.sh）：https://www.continuum.io/downloads
             安装视频：https://www.bilibili.com/video/av48846659?from=search&seid=6567236435893061639
+# added by Anaconda3 installer
+#export PATH="/home/jason/anaconda3/bin:$PATH"
 
                 
 opencv安装
